71b993960139e32c23c5f3c5018ea698e7d6d59b
0x10000 <mohamedaitsimhand@gmail.com>
2020-09-27 03:47:20 +0100

    Navbar has been added

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(() => {
+	// ====== [BEGIN] Toggle the sidebar on mobile version ======
+	const btnMenu = document.getElementById("menu-icon-trigger");
+	btnMenu.addEventListener("click", (e) => {
+		e.preventDefault();
+		document.querySelector('.menu-icon-wrapper').classList.toggle('open');
+		document.querySelector('.nav-content').classList.toggle('is-active');
+	});
+	// ====== [END] Toggle the sidebar on mobile version ======
+});
